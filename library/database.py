515f8ff43782484c47235b174761c10318e53afc
Sunil <sunilbaratam@stackbox.xyz>
2024-04-25 17:28:35 +0530

    Updated Code

---
+++ library/database.py
@@ -0,0 +1,53 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, event
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import relationship
+
+
+DATABASE_URL = "sqlite:///databases.db"
+
+
+engine = create_engine(DATABASE_URL)
+
+session = sessionmaker(bind=engine)
+session = session()
+
+Base = declarative_base()
+
+
+class Students(Base):
+    __tablename__ = "students"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    standard = Column(String)
+    roll_no = Column(Integer)
+    count_of_books = Column(Integer)
+
+
+class Books(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True)
+    book_name = Column(String)
+    author = Column(String)
+
+
+class Inventory(Base):
+    __tablename__ = "inventory"
+
+    id = Column(Integer, primary_key=True)
+    book_id = Column(Integer)
+    stock = Column(Integer, default=0)
+
+
+class Records(Base):
+    __tablename__ = "records"
+
+    id = Column(Integer, primary_key=True)
+    student_id = Column(String)
+    book_id = Column(String)
+    status = Column(String)
+
+
+Base.metadata.create_all(engine)
